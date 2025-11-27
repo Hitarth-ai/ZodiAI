ff4be14b5dd5b6b4b34129c750ffc492396a75e8
Hitarth-ai <hitarth452@gmail.com>
2025-11-27 19:11:11 +0530

    Update layout.tsx - Updated tab name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "ZodiAI",
+  description: "Your AI Panditji",
 };
 
 export default function RootLayout({
